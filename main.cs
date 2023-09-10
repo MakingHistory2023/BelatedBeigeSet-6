2310caaa1929811a3c0324ca9ddd0503a5a29cdf
MakingHistory2023 <143676019+MakingHistory2023@users.noreply.github.com>
2023-09-10 02:20:22 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Coming out of my cage");
+    Console.WriteLine ("And I've been doing just fine");
+    Console.WriteLine ("Gotta gotta be down");
+    Console.WriteLine (Because I want it all);
+  }
+}
